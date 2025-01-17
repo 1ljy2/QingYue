6b3f70e34b9b85aa871f053467be8827171ede4d
hy <2167313615@qq.com>
2025-01-17 22:30:06 +0800

    完成了侧边栏和顶栏工作，只剩填组件了
    首先，我阅读了原来开发的方式，发现学姐直接使用Layout写完了，很巧妙，我太笨了，只能用redux来传递折叠值了，重新封装了路由只剩填组件了

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
-import { Routes, Route } from 'react-router-dom'
-import routes from './router/router'
+import React from 'react';
+import { Provider } from 'react-redux';
+import { store, persistor } from './redux/store';
+import { PersistGate } from 'redux-persist/integration/react'; // 导入 PersistGate
+import IndexRouter from './router/IndexRouter';
+
 function App() {
   return (
-    <Routes>
-      {routes.map((route) => (
-        <Route key={route.path} path={route.path} element={route.element} />
-      ))}
-    </Routes>
-  )
+    <Provider store={store}>
+      <PersistGate
+        loading={null}
+        persistor={persistor}
+      >
+        <IndexRouter></IndexRouter>
+      </PersistGate>
+    </Provider>
+  );
 }
 
-export default App
+export default App;
--- src/components/Index/AiRouter.jsx
+++ src/components/Index/AiRouter.jsx
@@ -1,5 +1,38 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Home from '../../views/Index/home/Home';
+import Nopermission from '../../views/Index/nopermission/Nopermission';
+import Comprehensive from '../../views/Index/comprehension/Comprehensive';
+import Levelled from '../../views/Index/levelled/Levelled';
+import Record from '../../views/Index/record/Record';
+import Space from '../../views/Index/space/space';
+import Visualization from '../../views/Index/visualization/Visualization';
 
-export default function AiRouter() {
-  return <div>NewsRouter</div>;
+const LocalRouterMap = {
+  '/home': Home,
+  '*': Nopermission,
+  '/visualization': Visualization,
+  '/comprehension': Comprehensive,
+  '/levelled': Levelled,
+  '/record': Record,
+  '/space': Space,
+};
+
+function AiRouter() {
+  return (
+    <Routes>
+      {Object.keys(LocalRouterMap).map((path) => {
+        const Component = LocalRouterMap[path]; // 获取对应路径的组件
+        return (
+          <Route
+            key={path}
+            path={path}
+            element={<Component />}
+          />
+        );
+      })}
+    </Routes>
+  );
 }
+
+export default AiRouter;
